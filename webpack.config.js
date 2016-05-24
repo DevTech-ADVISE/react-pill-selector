be141173e22ce5a41f7bea3e6bf5261b2cbe2484
Stephen Smith <stephensmith@Stephens-MacBook-Pro.local>
2016-05-24 16:28:18 -0400

    Updating build process
    dependencies now actually dependencies and external

--- webpack.config.js
+++ webpack.config.js
@@ -35,11 +35,11 @@ module.exports = {
       commonjs2: 'react',
       amd: 'react',
     },
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom',
+    classnames: {
+      root: 'classnames',
+      commonjs: 'classnames',
+      commonjs2: 'classnames',
+      amd: 'classnames',
     },
   },
 
